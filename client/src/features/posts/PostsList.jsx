dd89c7381af8cf1e3d6bdc7cd155bf75c16baa87
Marc Valencia <marcrhanzelv@gmail.com>
2025-01-06 03:27:34 +0800

    Added Vite React client app

---
+++ client/src/features/posts/PostsList.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import { API_URL } from '../../constants';
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+    // Fetch posts from the API
+    useEffect(() => {
+      async function loadPosts() {
+        try {
+          const response = await fetch(API_URL);
+          if (response.ok) {
+            const posts = await response.json();
+            setPosts(posts);
+          } 
+          else {
+            throw response;
+          }
+        } catch (e) {
+          setError('An error occured!');
+          console.log('An error occured!', e);
+        } finally {
+          setLoading(false);
+        }
+      }
+      loadPosts();
+    }, []);
+
+    return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id} className='post-container'>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+    );
+  }
+
+export default PostsList;
